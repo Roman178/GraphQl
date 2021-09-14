77d76aa7e01f824ca5ef4a1ea35685dd03227d50
Roman <ismagilov771@gmail.com>
2021-09-14 22:49:50 +0300

    init

---
+++ server/index.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const { graphqlHTTP } = require("express-graphql");
+const cors = require("cors");
+const schema = require("./schema");
+const users = [{ id: 1, username: "Vasya", age: 18 }];
+
+const app = express();
+app.use(cors());
+
+const root = {
+  getAllUsers() {
+    return users;
+  },
+
+  getUser({ id }) {
+    return user.find((user) => user.id === id);
+  },
+};
+
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    graphiql: true,
+    schema,
+    rootValue: root,
+  })
+);
+
+app.listen(5000, () => console.log("server is running on port 5000"));
